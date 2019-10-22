48cf49c91b8e3ca0d2806ba91414daa860a7403a
arubinofaux <arubinofaux@gmail.com>
2019-10-21 20:10:24 -0400

    added example

---
+++ examples/messages.cr
@@ -0,0 +1,25 @@
+require "../src/twilio.cr"
+
+account_sid = ENV["TWILIO_ACCOUNT_SID"]
+auth_token = ENV["TWILIO_AUTH_TOKEN"]
+
+client = Twilio::Client.new(account_sid, auth_token)
+
+# test number 15005550006
+message = client.messages.create("+15005550006", "+15005550006", "Hey there!")
+
+puts message["sid"]
+
+fetchMessage = client.messages.fetch(message["sid"])
+
+puts fetchMessage
+
+# sleep to prevent,  
+# Cannot delete message because delivery has not been completed.
+sleep(5)
+redactMessage = client.messages.update(message["sid"]) 
+
+puts redactMessage
+
+sleep(1)
+client.messages.delete(message["sid"]) 
