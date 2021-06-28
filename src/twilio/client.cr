5a66d8d65c82cda4394600a968efb4451d961f6a
bovepm <bove@pm.me>
2021-06-28 10:49:35 +0200

    add http proxy support by using shard "spider-gazelle/connect-proxy"

--- src/twilio/client.cr
+++ src/twilio/client.cr
@@ -1,10 +1,12 @@
+require "connect-proxy"
+
 module Twilio
   class Client
     Host = URI.parse("https://api.twilio.com")
     getter account_sid
 
-    def initialize(@account_sid : String, auth_token)
-      @http_client = HTTP::Client.new(Host)
+    def initialize(@account_sid : String, auth_token, proxy = false)
+      @http_client = proxy ? ConnectProxy::HTTPClient.new(Host) : HTTP::Client.new(Host)
       @http_client.basic_auth(account_sid, auth_token)
     end
 
